14c54d8b7582bc49aeb137d7d30bd1dcac1dfc09
Nazar Pidhrushnyi <pidgrushnyi19@gmail.com>
2020-03-20 23:48:45 +0200

    feat: receive cities via Travelpayouts API

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* Данные */ 
+
+// Элементы формы
 const formSearch = document.querySelector('.form-search'),
   inputCitiesFrom = formSearch.querySelector('.input__cities-from'),
   dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from'),
@@ -6,18 +9,61 @@ const formSearch = document.querySelector('.form-search'),
   inputDateDepart = formSearch.querySelector('.input__date-depart'),
   buttonSearch = formSearch.querySelector('.button__search');
 
-const citiesArray = [
-  'Киев', 'Львов', 'Днепр', 'Вроцлав', 'Варшава',
-  'Москва', 'Санкт-Петербург', 'Минск', 'Одесса', 'Харьков'
-];
+const 
+// Получаем данные с сервера 
+  CITIES_API = 'http://api.travelpayouts.com/data/ru/cities.json',
+// Получаем данные с файла
+  // CITIES_API = 'data/cities.json',
+  PROXY = 'https://cors-anywhere.herokuapp.com/',
+  API_KEY = '81f67e92f1c6bf62ab4047b1fbcfef4a';
 
-// Отображение городов
-const showCity = (input, list) => {
+let citiesArray = [];
+
+/* /Данные */ 
+
+/* Функции */ 
+
+/**
+ * Запрос на получение данных
+ * Принимает адрес запроса и колбэк-функцию.
+ * Запрашивает данные по адресу url и, в случае успеха,
+ * передает полученные данные в колбэк
+*/
+const getData = (url, callback) => {
+  // Создаем объект на основе API XMLHttpRequest, встроенный в браузер
+  const request = new XMLHttpRequest();
+  
+  // Указываем тип запроса и url-адрес(куда будем отправлять)
+  request.open('GET', url);
+
+  // Обработчик события, чтобы не пропустить ответ
+  request.addEventListener('readystatechange', () => {
+    if(request.readyState !== 4) return;
+    
+    if(request.status === 200) {
+      callback(request.response);
+    } else {
+      console.error(request.status);
+    }
+  });
+
+  // Выполнить запрос
+  request.send();
+}
+
+/**
+ * Отображение городов, живой поиск
+ * Принимает инпут и список. Получает данные с инпута, 
+ * фильтрует массив городов 
+ * и наполняет список данными из отфильтрованного массива
+ */
+const showCitiesList = (input, list) => {
   list.textContent = '';
 
   if (input.value !== '') {
     const filterCity = citiesArray.filter((item) => {
-      const fixItem = item.toLowerCase();
+      const fixItem = item.name.toLowerCase();
+
       // Отсеять лишние города по первой букве
       if (input.value[0].toLowerCase() == fixItem[0]){
         return fixItem.includes(input.value.toLowerCase());
@@ -28,14 +74,34 @@ const showCity = (input, list) => {
     filterCity.forEach((item) => {
       const li = document.createElement('li');
       li.classList.add('dropdown__city');
-      li.textContent = item;
+      li.textContent = item.name;
       list.append(li);
     });
 
   }
 };
 
-// Проверить не совпадают ли направления
+/**
+ * Выбор города из выпадающего списка с автозаполнением поля ввода
+ * Принимает объект события, инпут и список.
+ * Проверет, происходит ли клик по элементу списка, 
+ * добавляет название выбранного города в инпут,
+ * обнуляет список
+ */
+const selectCity = (event, input, list) => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    input.value = target.textContent;
+    list.textContent = '';
+  }
+}
+
+/**
+ * Проверить не совпадают ли инпуты направлений
+ * Принимает инпуты городов отправления и прибытия.
+ * Проверет не пусты ли они,
+ * иначе отключаем кнопку и меняем ее цвет
+ */
 const compareCities = (departureCity, arrivalCity) => {
   if (departureCity.value !== '' || arrivalCity.value !== '') {
 
@@ -53,24 +119,19 @@ const compareCities = (departureCity, arrivalCity) => {
   }
 };
 
-// Выбор города из выпадающего списка с автозаполнением поля ввода
-const selectCity = (event, input, list) => {
-  const target = event.target;
-  if (target.tagName.toLowerCase() === 'li') {
-    input.value = target.textContent;
-    list.textContent = '';
-  }
-}
+/* /Функции */ 
+
+/* Обработчики событий */ 
 
 // Ввести: Откуда
 inputCitiesFrom.addEventListener('input', () => {
-  showCity(inputCitiesFrom, dropdownCitiesFrom);
+  showCitiesList(inputCitiesFrom, dropdownCitiesFrom);
   compareCities(inputCitiesFrom, inputCitiesTo);
 });
 
 // Ввести: Куда
 inputCitiesTo.addEventListener('input', () => {
-  showCity(inputCitiesTo, dropdownCitiesTo);
+  showCitiesList(inputCitiesTo, dropdownCitiesTo);
   compareCities(inputCitiesFrom, inputCitiesTo);
 });
 
@@ -85,3 +146,16 @@ dropdownCitiesTo.addEventListener('click', (event) => {
   selectCity(event, inputCitiesTo, dropdownCitiesTo);
   compareCities(inputCitiesFrom, inputCitiesTo);
 });
+
+/* /Обработчики событий */ 
+
+/* Вызовы функций */ 
+
+// Прокси для получение данных с сервера 
+getData(PROXY + CITIES_API, 
+  data => citiesArray = JSON.parse(data).filter(item => item.name));
+
+// Получение данных с файла 
+// getData(CITIES_API, data => citiesArray = JSON.parse(data).filter(item => item.name));
+
+/* /Вызовы функций */ 
